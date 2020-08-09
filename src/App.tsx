3ac4ec7c08afb6f8ecda5456ab569d1169782f9d
joshuasearle <joshryansearle@gmail.com>
2020-08-09 15:50:27 +1000

    Add wrapping div to App component for centering

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import Calculator from './components/Calculator';
+import classes from './css/classes';
 
 const App: React.SFC = () => {
-  return <Calculator />;
+  return (
+    <div className={classes.container}>
+      <Calculator />
+    </div>
+  );
 };
 
 export default App;
