c7bc07589286060c2211d2012fd702fea24eba3a
joshuasearle <joshryansearle@gmail.com>
2020-08-09 20:45:11 +1000

    Added stack logic

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,13 +1,36 @@
-import React, { useState, ReactText } from 'react';
+import React, { useState, ReactText, useEffect } from 'react';
 
 import Screen from './Screen';
 import Numpad from './Numpad';
 import classes from '../css/classes';
 
 const Calculator: React.SFC = () => {
-  const initialState: Array<string | number> = [];
+  const initialState: Array<string> = [];
   const [stack, setStack] = useState(initialState);
   console.log(stack);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeypress);
+    return () => {
+      window.removeEventListener('keydown', handleKeypress);
+    };
+  });
+
+  const handleKeypress = (e: any) => {
+    if (e.key !== 'Backspace' || stack.length === 0) return;
+    const top = stack[stack.length - 1];
+    const newStack = [...stack];
+
+    if (top.length === 1) {
+      newStack.pop();
+      setStack(newStack);
+    } else {
+      console.log(newStack);
+      const choppedTop = top.slice(0, top.length - 1);
+      newStack[newStack.length - 1] = choppedTop;
+      setStack(newStack);
+    }
+  };
 
   const reset = () => {
     const newStack: Array<string> = [];
@@ -15,8 +38,14 @@ const Calculator: React.SFC = () => {
   };
 
   const evaluate = () => {
+    const stackCopy: Array<string> = [...stack];
+    // remove last operator
+    const top = stackCopy[stackCopy.length - 1];
+    if (isNaN(+top) && top !== '(' && top !== ')') stackCopy.pop();
+
     try {
-      const result = eval(stack.join(' '));
+      const result = eval(stackCopy.join(''));
+
       const resultStack: Array<string> = [result.toString()];
       setStack(resultStack);
     } catch (e) {
@@ -25,23 +54,43 @@ const Calculator: React.SFC = () => {
   };
 
   const handleChar = (value: string) => {
-    const stackCopy = [...stack];
-    const n = stackCopy.length;
-    const top = stack[n - 1];
+    const top = stack[stack.length - 1];
+    const topIsNum = !isNaN(+top);
+    const valueIsNum = !isNaN(+value);
 
-    if (!isNaN(+value) && !isNaN(+top)) {
-      stackCopy[n - 1] = top + value;
-    } else if (value === '.') {
-      stackCopy[n - 1] = top + value;
-    } else {
-      stackCopy.push(value);
-    }
+    if (value === '.' && !valueIsNum) return;
+    if (value === '(' && topIsNum && top !== ')') return;
+    if (value === ')' && !topIsNum && top !== ')') return;
+    if (!topIsNum && !valueIsNum && value !== '(' && value !== ')') return;
+
+    const stackCopy = [...stack];
+
+    stackCopy.push(value);
     setStack(stackCopy);
+  };
+
+  const formatOutput = () => {
+    let output = [];
+    for (let i = 0; i < stack.length; i++) {
+      let top: string = output[output.length - 1];
+      let current = stack[i];
+      let topIsNum = !isNaN(+top);
+      let currentIsNum = !isNaN(+current);
+
+      if (i === 0) {
+        output.push(stack[i]);
+      } else if (topIsNum && currentIsNum) {
+        output[output.length - 1] = top + current;
+      } else {
+        output.push(current);
+      }
+    }
+    return output.join(' ');
   };
 
   return (
     <div className={classes.calculator}>
-      <Screen output={stack.join(' ')} />
+      <Screen output={formatOutput()} />
       <Numpad handleChar={handleChar} reset={reset} evaluate={evaluate} />
     </div>
   );
