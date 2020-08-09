210bd2fd9bcd5427653a1a0e90c135ca83725580
joshuasearle <joshryansearle@gmail.com>
2020-08-09 17:57:23 +1000

    Add buttons to numpad in order

--- src/components/Numpad.tsx
+++ src/components/Numpad.tsx
@@ -1,7 +1,79 @@
 import * as React from 'react';
- 
-const Numpad: React.SFC = () => {
-  return <h1>Numpad</h1>;
+
+import classes from '../css/classes';
+import Button, { ButtonTypes } from './Button';
+
+interface NumpadProps {
+  handleChar: (value: string) => void;
+  reset: () => void;
+  evaluate: () => void;
 }
- 
+
+const Numpad: React.SFC<NumpadProps> = ({ handleChar, reset, evaluate }) => {
+  const buttons = [
+    '(',
+    ')',
+    '%',
+    'AC',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    '*',
+    '1',
+    '2',
+    '3',
+    '-',
+    '.',
+    '0',
+    '=',
+    '+',
+  ];
+
+  return (
+    <div className={classes.numpad}>
+      {buttons.map((value) => {
+        if (value === 'AC') {
+          return (
+            <Button
+              key={value}
+              onClick={reset}
+              buttonType={ButtonTypes.Operator}
+            >
+              {value}
+            </Button>
+          );
+        }
+
+        if (value === '=') {
+          return (
+            <Button
+              key={value}
+              onClick={evaluate}
+              buttonType={ButtonTypes.Other}
+            >
+              {value}
+            </Button>
+          );
+        }
+
+        return (
+          <Button
+            key={value}
+            onClick={handleChar}
+            buttonType={
+              !isNaN(+value) ? ButtonTypes.Number : ButtonTypes.Operator
+            }
+          >
+            {value}
+          </Button>
+        );
+      })}
+    </div>
+  );
+};
+
 export default Numpad;
